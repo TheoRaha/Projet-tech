491a43a6ad1d718532fcb05155eaed3a050dc5f0
The10axe <the10axe@gmail.com>
2024-03-25 14:30:35 +0100

    Fix Ray ON A REUSSI LES BGS

--- Assets/Heuristique.cs
+++ Assets/Heuristique.cs
@@ -24,9 +24,25 @@ public class Heuristique : MonoBehaviour
     void Update()
     {
         value = 0;
+        if (!detectCollision(joueur, joueurAutre))
+            value += 1000;
         if(joueur != null && joueurAutre != null){
             value += (1/Math.Sqrt((joueur.transform.position.x-joueurAutre.transform.position.x)*(joueur.transform.position.x-joueurAutre.transform.position.x)+(joueur.transform.position.z - joueurAutre.transform.position.z)*(joueur.transform.position.z - joueurAutre.transform.position.z)))*100;
         }
         boiteTexte.GetComponent<TMPro.TextMeshProUGUI>().text = value.ToString();
+    }
+
+    Boolean detectCollision(GameObject joueur, GameObject autreJoueur){
+        Vector3 direction = autreJoueur.transform.position - joueur.transform.position;
+
+        RaycastHit[] hits = Physics.RaycastAll(joueur.transform.position, direction, Vector3.Distance(joueur.transform.position, autreJoueur.transform.position));
+        foreach (RaycastHit hit in hits)
+        {
+            if (hit.collider.gameObject != autreJoueur)
+            {
+                return true;
+            }
+        }
+        return false;
     }
 }
